ff5bbe084be157dcc5c7c7e70c3ed798b081baaa
Chelsa MJ <113827875+ChelsaMJ@users.noreply.github.com>
2024-09-08 23:37:11 +0530

    Create for1.cpp

---
+++ for1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>  
+using namespace std;  
+
+int main() {  
+    int n = 10;  
+
+    for(int i=1; i<=n; i=i+2) {  
+        cout << i << " "; //1 3 5 7 9  
+    }  
+
+    cout << endl;  
+    return 0;  
+}
