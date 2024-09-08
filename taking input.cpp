686a9a0d038b904e1d6e7c99f5c655c64439bd2c
Chelsa MJ <113827875+ChelsaMJ@users.noreply.github.com>
2024-09-08 22:34:10 +0530

    Create taking input.cpp

---
+++ taking input.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int age; //print random garbage value
+    
+    cout<<"Enter your age: ";
+    cin>>age;
+    
+    cout<<"Age given is: "<<age<<endl;
+    
+
+    return 0;
+}
