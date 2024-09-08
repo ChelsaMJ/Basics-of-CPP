5d36cda3ece4669c62d0a2cc12b759bf2859019e
Chelsa MJ <113827875+ChelsaMJ@users.noreply.github.com>
2024-09-08 22:53:50 +0530

    Create logical operators.cpp

---
+++ logical operators.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout<<((3>2)&&(3>4))<<endl; //logical And
+    cout<<((3>2)||(3>1))<<endl; //logical or
+    cout<<!(3>2)<<endl;//logical not !
+    cout<<!(3<2)<<endl;
+    
+
+    return 0;
+}
