6d75b0b0898c1a600f11d58e3bd21c5cd45318ba
Chelsa MJ <113827875+ChelsaMJ@users.noreply.github.com>
2024-09-08 22:27:36 +0530

    Create Implicit Conversion.cpp

---
+++ Implicit Conversion.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+//implicit conv --> small dt to bigger dt
+
+int main() {
+    
+    char grade='a';
+    char grade1='A';
+    
+    int value=grade;
+    int value1=grade1;
+    
+    cout<<value<<endl;
+    cout<<value1<<endl;
+    
+
+    return 0;
+}
