8c48db67ed37a140930d4871fb95de8a79d3ea02
Chelsa MJ <113827875+ChelsaMJ@users.noreply.github.com>
2024-09-08 22:31:05 +0530

    Create TypeCast - ET.cpp

---
+++ TypeCast - ET.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+//explicit type cast --> big to small
+
+int main() {
+    
+    double price = 100.99;
+    int newP = (int)price;
+  
+    
+    cout<<newP<<endl;
+    
+
+    return 0;
+}
